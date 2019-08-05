d1eaa023cca84afc6450f1e4e88b52aece6f3d91
rohinilokare <rohini@treeni.com>
2019-08-05 20:46:22 +0530

    feature::RubyOnRails::AngularJS::created rails app for student Management System and integreted it with AngularJS::only connectivity done.

---
+++ studentManagementSystem/app/assets/javascripts/app.js
@@ -0,0 +1,12 @@
+angular
+ .module('app', ['ui.router', 'templates'])
+ .config(['$stateProvider', '$urlRouterProvider',
+ function($stateProvider, $urlRouterProvider) {
+ $stateProvider
+  .state('home', {
+  url: '/home',
+  templateUrl: 'main/_home.html',
+  controller: 'MainCtrl'
+  })
+  $urlRouterProvider.otherwise('home');
+ }])
---
+++ studentManagementSystem/app/assets/javascripts/main/MainCtrl.js
@@ -0,0 +1,5 @@
+function MainCtrl() {
+}
+angular
+ .module('app')
+ .controller("MainCtrl", MainCtrl);
